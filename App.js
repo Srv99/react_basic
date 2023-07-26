25900997e4b99b67c166b6b2decea4cafdf62003
Suman Saurav <ssaurav5759@gmail.com>
2023-07-27 00:17:05 +0530

    basic_01

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading3 = React.createElement("h3", {}, "This is heading 1");
+const heading2 = React.createElement("h2", {}, "This is heading 2");
+const heading = React.createElement("h1", { id: "heading" }, [
+  "Hello world!",
+  heading2,
+  heading3,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
